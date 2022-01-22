f083314f126db02d0ff68fa51095628e4979081b
Anastasia Shatokhina <shnatya@gmail.com>
2022-01-21 21:58:26 -0800

    commit tree 75bddee327522d001f9b0a40b634ac71a7a4e9e1

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(arg)
+    while arg > 0
+        puts "#{arg} SECOND(S)!"
+        arg -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(arg)
+    while arg > 0
+        puts "#{arg} SECOND(S)!"
+        arg -= 1
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"      
 end
